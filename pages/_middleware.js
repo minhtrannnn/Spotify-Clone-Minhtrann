b916ea1e4c76353bf6a1b32f6795868ea334f252
minhtrannnn <103587659+minhtrannnn@users.noreply.github.com>
2022-04-28 21:17:17 -0400

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,18 +1,18 @@
 import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req) {
     const token = await getToken({ req, secret:process.env.JWT_SECRET });
 
     const { pathname } = req.nextUrl;
-    const url = req.nextUrl.clone();
+    const url = req.nextUrl;
+    url.pathname = "/login";
     
     if(pathname.includes("/api/auth") || token) {
         return NextResponse.next();
     }
 
     if(!token && pathname !== "/login") {
-        url.pathname = "/login";
         return NextResponse.redirect(url);
     } 
  }
