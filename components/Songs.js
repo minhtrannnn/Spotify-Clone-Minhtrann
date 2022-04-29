a0c27caafe532a32046c11d7aaf71a287750e36e
minhtrannnn <103587659+minhtrannnn@users.noreply.github.com>
2022-04-29 01:27:28 -0400

    Update Songs.js

--- components/Songs.js
+++ components/Songs.js
@@ -4,10 +4,21 @@ import Song from "./Song";
 
 function Songs() {
     const playlist = useRecoilValue(playlistState);
-
-    console.log(playlist);
+    
     return (    
         <div className= "px-8 flex flex-col space-y-1 pb-28 text-white">
+            <div className="grid grid-cols-2 text-gray-400 py-2 px-5 hover:bg-gray-900 cursor-pointer">
+                <div className="flex items-center space-x-4">
+                    <p>#</p>
+                    <h2>Title</h2>
+                </div>
+
+                <div className="flex items-center justify-between ml-auto md:ml-0">
+                    <h2>Album</h2>
+                    <h2>Time</h2>
+                </div>
+            </div>
+            <hr className="w-full border-t-[0.1px] border-gray-600"/>
             {playlist?.tracks.items.map((track, i) => (
                 <Song key={track.track.id} track={track} order={i} />
             ))}
